7f4ea11d4f7ff800e9c4c3598e899b15dae3c002
Ivan Ricardo Lopes <ivan.lopes@xpi.com.br>
2021-01-30 14:50:53 -0300

    integrator classes

---
+++ Core/Concrete/BotIntegrator.cs
@@ -0,0 +1,14 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Core.Concrete
+{
+    public class BotIntegrator
+    {
+        public void Notify(IEnumerable<SyntheticTestResult> enumerable)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Concrete/ZabbixIntegrator.cs
@@ -0,0 +1,13 @@
+﻿using Core.Models;
+using System;
+
+namespace Core.Concrete
+{
+    public class ZabbixIntegrator
+    {
+        public void AckAlert(NocAlert nocAlert)
+        {
+            Console.WriteLine("=============== End of process, hit any key to finish ===============");
+        }
+    }
+}
